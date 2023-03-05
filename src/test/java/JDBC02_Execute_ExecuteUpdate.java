47937c6f95441570e7f72c37999dbba9f56ac38f
Betul Alptekin <f16btl@gmail.com>
2023-03-05 16:44:32 +0300

    v2

--- src/test/java/JDBC02_Execute_ExecuteUpdate.java
+++ src/test/java/JDBC02_Execute_ExecuteUpdate.java
@@ -170,10 +170,5 @@ public class JDBC02_Execute_ExecuteUpdate {
         }
     }
 
-        git init
-        git add README.md
-        git commit -m "first commit"
-        git branch -M main
-        git remote add origin https://github.com/fbetulalptekin/JDBC-mySQL.git
-        git push -u origin main
+
 
